48b40cd94dde3f13d4cbbd00359f35f29b5a3878
iojvan <ivos.ojvan@gmail.com>
2025-08-17 15:56:51 +0200

    Bug fix in extractAllClaims method, change parseClaimsJwt to parseClaimsJws.

--- src/main/java/com/iso/todos/service/JwtServiceImpl.java
+++ src/main/java/com/iso/todos/service/JwtServiceImpl.java
@@ -37,7 +37,7 @@ public class JwtServiceImpl implements JwtService{
         return Jwts.parserBuilder()
                 .setSigningKey(getSigningKey())
                 .build()
-                .parseClaimsJwt(token)
+                .parseClaimsJws(token)
                 .getBody();
     }
 
